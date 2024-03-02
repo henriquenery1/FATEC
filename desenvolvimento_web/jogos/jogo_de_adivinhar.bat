6d078bdec4431e67569d085186246ed7ddb73b15
Henrique <henriquenery07@gmail.com>
2024-03-02 16:54:19 -0300

    feat: Adicionando função de verificação de palpites e tratamento de erros

--- desenvolvimento_web/jogos/jogo_de_adivinhar.bat
+++ desenvolvimento_web/jogos/jogo_de_adivinhar.bat
@@ -25,4 +25,61 @@ color 0b
     echo.
     set /p palpite=Digite o seu palpite: 
     set /a primeiro_palpite = %palpite%
-    goto:jogada
+    goto:verificar_palpite
+
+
+:verificar_palpite
+
+    if %palpite% GTR 50 (
+        echo.
+        echo ------------------------------------------------------
+        echo   Opcao Invalida! Digite um numero menor que 50
+        echo ------------------------------------------------------
+        pause
+        cls
+        goto:palpite
+        
+    ) 
+
+    if %palpite% LSS 1 (
+        echo.
+        echo ------------------------------------------------------
+        echo   Opcao Invalida! Digite um numero maior que 1
+        echo ------------------------------------------------------
+        pause
+        cls
+        goto:palpite
+    ) 
+           
+    if %palpite% == %numero_sorteado% (
+        echo.
+        echo ------------------------------------------------------
+        echo             PARABENS, VOCE ACERTOU!
+        echo ------------------------------------------------------
+        echo. 
+        pause
+        cls
+        goto:sair     
+    )   
+
+    if %palpite% LSS %numero_sorteado% (
+        echo.
+        echo ------------------------------------------------------
+        echo              Digite um numero maior
+        echo ------------------------------------------------------
+        set /a tentativas=%tentativas% - 1
+        pause
+        cls
+        goto:palpite
+    )
+    
+    if %palpite% GTR %numero_sorteado% (
+        echo.
+        echo ------------------------------------------------------
+        echo              Digite um numero menor
+        echo ------------------------------------------------------
+        set /a tentativas=%tentativas% - 1
+        pause
+        cls
+        goto:palpite
+    )
