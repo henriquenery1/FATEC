90c346b54804fe5835f7c2328ef2d7835ec077c0
Henrique <henriquenery07@gmail.com>
2024-03-05 15:46:30 -0300

    feat: Implementa a lógica principal do jogo

--- desenvolvimento_web/jogos/jokenpo.bat
+++ desenvolvimento_web/jogos/jokenpo.bat
@@ -40,6 +40,7 @@ title Jo-Ken-Po
 
 :regras
     cls
+    echo.
     echo                          ( o o )
     echo        +------------.oooO--(_)--Oooo.-------------+
     echo        -                                          -
@@ -85,3 +86,92 @@ title Jo-Ken-Po
     echo --------------------
     echo   Opcao invalida!
     echo --------------------
+    goto :eof
+
+:game
+    cls
+
+    call :exibir_opcoes_jogador
+
+    set /p escolha_jogador=Digite sua escolha: 
+
+    set /a escolha_computador=(%random% %% 5) + 1
+
+    if %escolha_jogador% == 6 goto:menu
+    if %escolha_jogador% lss 1 goto:game
+    if %escolha_jogador% gtr 5 goto:game
+
+    if %escolha_jogador% == 1 set escolha_jogador_txt=Pedra
+    if %escolha_jogador% == 2 set escolha_jogador_txt=Papel
+    if %escolha_jogador% == 3 set escolha_jogador_txt=Tesoura
+    if %escolha_jogador% == 4 set escolha_jogador_txt=Lagarto
+    if %escolha_jogador% == 5 set escolha_jogador_txt=Spock
+
+    if %escolha_computador% == 1 set escolha_computador_txt=Pedra
+    if %escolha_computador% == 2 set escolha_computador_txt=Papel
+    if %escolha_computador% == 3 set escolha_computador_txt=Tesoura
+    if %escolha_computador% == 4 set escolha_computador_txt=Lagarto
+    if %escolha_computador% == 5 set escolha_computador_txt=Spock
+
+    echo.
+    echo Voce escolheu: %escolha_jogador_txt%
+    echo O computador escolheu: %escolha_computador_txt%
+    echo.
+
+    if "%escolha_jogador_txt%" == "%escolha_computador_txt%" (
+    echo Empate!
+    ) else if "%escolha_jogador_txt%" == "Pedra" (
+        if "%escolha_computador_txt%" == "Tesoura" (
+            echo Voce ganhou!
+        ) else if "%escolha_computador_txt%" == "Lagarto" (
+            echo Voce ganhou!
+        ) else (
+            echo Computador ganhou!
+        )
+    ) else if "%escolha_jogador_txt%" == "Papel" (
+        if "%escolha_computador_txt%" == "Pedra" (
+            echo Voce ganhou!
+        ) else if "%escolha_computador_txt%" == "Spock" (
+            echo Voce ganhou!
+        ) else (
+            echo Computador ganhou!
+        )
+    ) else if "%escolha_jogador_txt%" == "Tesoura" (
+        if "%escolha_computador_txt%" == "Papel" (
+            echo Voce ganhou!
+        ) else if "%escolha_computador_txt%" == "Lagarto" (
+            echo Voce ganhou!
+        ) else (
+            echo Computador ganhou!
+        )
+    ) else if "%escolha_jogador_txt%" == "Lagarto" (
+        if "%escolha_computador_txt%" == "Spock" (
+            echo Voce ganhou!
+        ) else if "%escolha_computador_txt%" == "Papel" (
+            echo Voce ganhou!
+        ) else (
+            echo Computador ganhou!
+        )
+    ) else if "%escolha_jogador_txt%" == "Spock" (
+        if "%escolha_computador_txt%" == "Tesoura" (
+            echo Voce ganhou!
+        ) else if "%escolha_computador_txt%" == "Pedra" (
+            echo Voce ganhou!
+        ) else (
+            echo Computador ganhou!
+        )
+    )
+
+    echo.
+    pause
+    goto:menu
+
+:exibir_opcoes_jogador
+    echo.
+    echo [1] Pedra
+    echo [2] Papel
+    echo [3] Tesoura
+    echo [4] Lagarto
+    echo [5] Spock
+    echo [6] Voltar ao menu principal
+    echo.
