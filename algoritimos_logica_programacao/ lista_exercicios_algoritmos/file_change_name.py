ea8eb3c02ac0b1b652b979f8267ac7b69b7d621c
Henrique <henriquenery07@gmail.com>
2024-03-12 23:37:03 -0300

    feat: Adiciona script para mudar o nome dos arquivos e adicionar o nome do aluno

--- algoritimos_logica_programacao/ lista_exercicios_algoritmos/file_change_name.py
+++ algoritimos_logica_programacao/ lista_exercicios_algoritmos/file_change_name.py
@@ -1,8 +1,31 @@
 import os
+import shutil
+import re
 
-def list_files():
-    files = []
-    files = os.system ("ls")
-    name = input()
-    for i in files:
-        os.system(f"mv {i}_{name}")
+def copy_and_rename_files_with_new_name():
+    new_name = input("Insira o seu nome para os arquivos: ")
+
+    new_folder = "arquivos_renomeados"
+    os.makedirs(new_folder, exist_ok=True)
+
+    files = os.listdir()
+    
+    # Filtra apenas os arquivos que seguem o padrão "exXX.alg"
+    exercise_files = [file for file in files if re.match(r'ex\d{2}\.alg', file)]
+
+    for file in exercise_files:
+        # Extrai o número do exercício do nome do arquivo
+        ex_number = re.search(r'ex(\d{2})\.alg', file).group(1)
+
+        # # Renomeia o arquivo no formato "exXX_new_name.alg"
+        new_file_name = f"ex{ex_number}_{new_name}.alg"
+
+        shutil.copy(file, os.path.join(new_folder, new_file_name))
+
+    print("Arquivos renomeados com sucesso.")
+
+def main():
+    copy_and_rename_files_with_new_name()
+
+if __name__ == "__main__":
+    main()
