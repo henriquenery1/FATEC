aa1fcd7d357506512c275e0db968629da4f5886e
Henrique <henriquenery07@gmail.com>
2024-04-09 19:49:19 -0300

    feat: renomeando arquivos e criando pasta para entrega da prova

---
+++ algoritimos_logica_programacao/p1/rename_exercise_files.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+import re
+
+def copy_and_rename_files_with_new_name():
+    new_name = input("Insira o seu nome para os arquivos: ")
+
+    new_folder = input("Insira o seu nome da pasta: ")
+    os.makedirs(new_folder, exist_ok=True)
+
+    files = os.listdir()
+    
+    # Filtra apenas os arquivos que seguem o padrão "exXX.alg"
+    exercise_files = [file for file in files if re.match(r'ex\d{2}\.alg', file)]
+
+    for file in exercise_files:
+        # Extrai o número do exercício do nome do arquivo
+        ex_number = re.search(r'ex(\d{2})\.alg', file).group(1)
+
+        new_file_name = f"ex{ex_number}_{new_name}.alg"
+
+        shutil.copy(file, os.path.join(new_folder, new_file_name))
+
+    print("Arquivos renomeados com sucesso.")
+
+if __name__ == "__main__":
+    copy_and_rename_files_with_new_name()
